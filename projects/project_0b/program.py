da95cdbcfa920fbbb34a35dfbd6489aefbf00147
AbhinavT2 <abhinav.tumu1@gmail.com>
2019-08-31 13:22:18 -0700

    project_0b init

---
+++ projects/project_0b/program.py
@@ -0,0 +1,20 @@
+val = input("enter: ")
+
+two_plus = "+-+"
+two_line = "| |"
+three_plus = "+-+-+"
+
+
+def diagonal_block(n):
+    if (n == 1):
+        print("TEST")
+        print(two_plus)
+        print(two_line)
+        print(two_plus)
+    print(n)
+
+diagonal_block(val)
+
+
+
+
