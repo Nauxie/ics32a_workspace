299779f1920bb06f6c154c980a917602cbdc9784
Abhinav  Tumu <abhinav@dhcp-10-8-015-009.mobile.reshsg.uci.edu>
2019-10-14 14:00:01 -0700

    final draft

--- projects/project_1/project1.py
+++ projects/project_1/project1.py
@@ -1,34 +1,33 @@
 from pathlib import Path
 import os
-path_input = input()
-eligible_paths = []
-interesting_paths = []
+eligible_paths = [] # store eligible paths
+interesting_paths = [] # store interesting paths
 
 
-def sort_iterdir(gen: [list]) -> list:
+def sort_iterdir(input_path_list: [list]) -> list: 
     ''' Turns the list casted iterdir generator Into a sorted list of Paths '''
-    list2 = []
-    for path in gen:
-        list2.append(path.as_posix())
-    list2.sort()
-    list3 = []
-    for string in list2:
-        list3.append(Path(string))
-    return list3
+    string_list = [] 
+    for path in input_path_list:
+        string_list.append(path.as_posix()) # turns list of Paths into list of strings
+    string_list.sort() # sorts that list of strings
+    path_list = []
+    for string in string_list:
+        path_list.append(Path(string)) # turns list of strings back into (sorted) list of Paths
+    return path_list
 
 def regularDirect(inpt: [str]) -> None:
     '''Prints out all the files in a  directory'''
     if (inpt.startswith('D ')):  # path starts with D (directory)
-        inpt = inpt[2:]
-        my_path = Path(inpt)
-        if (my_path.exists()):
+        inpt = inpt[2:] # gets rid of the D 
+        my_path = Path(inpt) 
+        if (my_path.exists()): # only make path eligible if it exists
             for subpath in sort_iterdir(list(my_path.iterdir())):
-                if(subpath.is_file()):
+                if(subpath.is_file()): # if subpath is file, print and make eligible
                     print(subpath)
-                    eligible_paths.append(subpath.as_posix())
+                    eligible_paths.append(subpath)
         else:
             print('ERROR')
-            regularDirect(input())
+            regularDirect(input()) # restart input
         
 
     else:
@@ -38,23 +37,23 @@ def regularDirect(inpt: [str]) -> None:
 
 def specialDirect(inpt: [str]) -> None:
     '''Prints out all files in directory including subdirectories'''
-    if (inpt.startswith('R ')):  # path starts with R (directory)
-        inpt = inpt[2:]
+    if (inpt.startswith('R ')):  # path starts with R (directory) 
+        inpt = inpt[2:] # removes R 
         my_path = Path(inpt)
-        if (my_path.exists()):
+        if (my_path.exists()): # makes file path eligible if exists
             for subpath in sort_iterdir(list(my_path.iterdir())):
-                if(subpath.is_file()):
+                if(subpath.is_file()): # if file, make eligible
                     print(subpath)
-                    eligible_paths.append(subpath.as_posix())
+                    eligible_paths.append(subpath)
 
-            for subpath in sort_iterdir(my_path.iterdir()):
+            for subpath in sort_iterdir(my_path.iterdir()): # if directory, recurisvely call method on new subdirectory
                 if(subpath.is_dir()):
                     specialDirect('R ' + subpath.as_posix())
         else:
             print('ERROR')
             specialDirect(input())
 
-    elif (inpt.startswith('D ')):
+    elif (inpt.startswith('D ')): # redirects to D method (regularDirect())
         regularDirect(inpt)
     else:
         print('ERROR')
@@ -62,55 +61,53 @@ def specialDirect(inpt: [str]) -> None:
 
 
 def narrow_interesting(interest: [str]) -> None:
-    '''narrows the interesting files based off of letter'''
+    '''narrows the interesting files based off of input'''
     if (interest == 'A'):
-        for string in eligible_paths:
-            print(string)
-            interesting_paths.append(string)
-    elif (interest.startswith('N ')):
+        for path in eligible_paths: # adds all elgible paths to interesting paths
+            print(path)
+            interesting_paths.append(path) 
+    elif (interest.startswith('N ')): 
         interest = interest[2:]
-        for string in eligible_paths:
-            if (string.endswith(interest)):
-                print(string)
-                interesting_paths.append(string)
+        for path in eligible_paths: # if filename matches input, make interesting
+            if (path.as_posix()[path.as_posix().rfind('/')+1:] == interest): #checks if the string after the last '/' matches the file
+                print(path)
+                interesting_paths.append(path)
     elif (interest.startswith('E ')):
         interest = interest[2:]
-        for string in eligible_paths:
-            if (string[string.find('.'):] == interest or string[string.find('.')+1:] == interest):
-                print(string)
-                interesting_paths.append(string)
-    elif (interest.startswith('T ')):
+        for path in eligible_paths: # if extension matches input, make interesting
+            if (path[path.find('.'):] == interest or path[path.find('.')+1:] == interest): #checks if the text after or including and after the dot in the path matches
+                print(path)
+                interesting_paths.append(path) 
+    elif (interest.startswith('T ')): 
         interest = interest[2:]
-        uniquefiles = []
-        for string in eligible_paths:
+        uniquefiles = [] # keep track of which files have been printed so even if there are multiple occurences of text, it will only be printed once
+        for path in eligible_paths:
             try:
-                my_file = open(string, 'r')
+                my_file = open(path, 'r')
                 # print(my_file)
                 for line in my_file:
-                    if (interest in line):
-                        if (string not in uniquefiles):
-                            print(string)
-                            interesting_paths.append(string)
-                            uniquefiles.append(string)
+                    if (interest in line): # if input is on any line
+                        if (path not in uniquefiles): #only if this file hasn't been counted as having the input text, then print and make interesting
+                            print(path)
+                            interesting_paths.append(path)
+                            uniquefiles.append(path)
 
-            except UnicodeDecodeError:
+            except UnicodeDecodeError: # if file cannot be opened, then ignore it (pass it)
                 pass
     elif (interest.startswith('< ')):
         interest = interest[2:]
-        interest = int(interest)
-        for string in eligible_paths:
-            size = Path(string).stat().st_size
-            if (size < interest):
-                print(string)
-                interesting_paths.append(string)
+        for path in eligible_paths:
+            size = path.stat().st_size # size of file
+            if (size < interest): 
+                print(path)
+                interesting_paths.append(path)
     elif (interest.startswith('> ')):
-        interest = interest[2:]
-        interest = int(interest)
-        for string in eligible_paths:
-            size = Path(string).stat().st_size
+        interest = int(interest[2:])
+        for path in eligible_paths:
+            size = path.stat().st_size
             if (size > interest):
-                print(string)
-                interesting_paths.append(string)
+                print(path)
+                interesting_paths.append(path)
     else:
         print('ERROR')
         narrow_interesting(input())
@@ -118,25 +115,26 @@ def narrow_interesting(interest: [str]) -> None:
 def take_action(action: [str]) -> None:
     '''Acts upon interesting directories based off of input'''
     if (action == 'F'):
-        for string in interesting_paths:
+        for path in interesting_paths:
             try:
-                my_file = open(string, 'r')
-                print(my_file.readline().rstrip())
+                my_file = open(path, 'r')
+                print(my_file.readline().rstrip()) # prints first line of file
 
-            except UnicodeDecodeError:
+            except UnicodeDecodeError: # prints NOT TEXT if file cannot be opened
                 print('NOT TEXT')
     elif(action == 'D'):
-        for string in interesting_paths:
-            my_file = open(string+'.dup', 'w+')
+        for path in interesting_paths:
+            my_file = open(path.as_posix()+'.dup', 'w+') # writes new file with .dup extension
 
     elif(action == 'T'):
-        for string in interesting_paths:
-            Path(string).touch()
+        for path in interesting_paths:
+            path.touch() # update timestamp on file
     else:
         print('ERROR')
-        take_action(input())
+        take_action(input()) 
 
-#begin main sequence
+#begin main running sequence
+path_input = input()
 specialDirect(path_input) 
 interesting = input()
 narrow_interesting(interesting)
@@ -145,5 +143,5 @@ if (len(interesting_paths) != 0):
     take_action(change)
 # end main sequence
 
-if __name__ == '__main__':
+if __name__ == '__main__': #test module
     print('hello')
